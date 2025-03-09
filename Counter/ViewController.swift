80e14458bdd61f14f9784c376f176976586813a0
Alexey <aratusha2602@gmail.com>
2025-03-09 17:40:49 +0300

    Исправлена ошибка с отображением количества строк в textView

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -17,15 +17,12 @@ final class ViewController: UIViewController {
     
     var historyActions: [String] = [] { didSet {
         updateHistoryTextView()
-        var sum = 0
-        for i in 0...historyActions.count {
-        sum += i
-        }
-        if sum > 8 {
+        if historyActions.count > 15 {
             historyActions.removeFirst()
         }
-        
     }}
+    
+  
     
     
     
@@ -46,13 +43,13 @@ final class ViewController: UIViewController {
         }
     
     @IBAction func resetCounterValue(_ sender: Any) {
-        var data1 = currentDate()
+        let data1 = currentDate()
         counterNumber = 0
         historyActions.append("\(data1): Зачение счетчика сброшено\n")
     }
     
     @IBAction func decreaseСounterValue(_ sender: Any) {
-        var data1 = currentDate()
+       let data1 = currentDate()
         if counterNumber > 0 {
             counterNumber -= 1 }
         historyActions.append("\(data1): Зачение счетчика изменено: -1\n")
@@ -60,13 +57,13 @@ final class ViewController: UIViewController {
     
     @IBAction func addValueCounter(_ sender: Any) {
         counterNumber += 1
-        var data1 = currentDate()
+        let data1 = currentDate()
         historyActions.append("\(data1): Зачение счетчика изменено: +1\n")
     }
     
     private func currentDate() -> String {
         let dateNow = Date()
-        var dateFormatter = DateFormatter()
+        let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "HH:mm:ss"
         let dateCurrent = dateFormatter.string(from: dateNow)
         return dateCurrent
